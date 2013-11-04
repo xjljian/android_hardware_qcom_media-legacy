6b72e3516aea6fdb89d3931753be3457c39396da
Kra1o5 <kra1o5x@gmail.com>
2013-11-04 14:08:46 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,5 +1,12 @@
 ifeq ($(TARGET_QCOM_MEDIA_VARIANT),legacy)
+QCOM_MEDIA_ROOT := $(call my-dir)
+ifneq ($(filter msm8974 msm8960 msm8660 msm7x27a msm7x27 msm7x30,$(TARGET_BOARD_PLATFORM)),)
+include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
+include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
+endif
 
-include $(call all-subdir-makefiles)
-
+ifneq ($(filter msm8974 msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
+include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
+include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
+endif
 endif
